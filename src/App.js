b663453b0bf8e771c7b9eb56ede87f1e24245bad
shermzlim <shermz.lim.jh@gmail.com>
2020-12-27 14:13:47 +0800

    Enable navigation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
-import Navbar from "./Navbar";
-import Experience from "./Experience";
-import CP from "./CP";
-import Project from "./Project";
-import Teaching from "./Teaching";
-import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Experience from './Experience';
+import CP from './CP';
+import Project from './Project';
+import Teaching from './Teaching';
+import Home from './Home';
 
 const App = () => {
-    return (
-        <div>          
-          <Router>            
-            <div>
-              <Navbar />              
-              <Switch>
-                <Route path="/experience">
-                  <Experience />
-                </Route>
-                <Route path="/cp">
-                  <CP />
-                </Route>              
-                <Route path="/projects">
-                  <Project />
-                </Route>
-                <Route path="/teaching">
-                  <Teaching />
-                </Route>        
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </div>
-          </Router>            
+  return (
+    <div>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route path="/experience">
+              <Experience />
+            </Route>
+            <Route path="/cp">
+              <CP />
+            </Route>
+            <Route path="/projects">
+              <Project />
+            </Route>
+            <Route path="/teaching">
+              <Teaching />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         </div>
-    )
-}
+      </Router>
+    </div>
+  );
+};
 
 export default App;
